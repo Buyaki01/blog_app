e2c7ecb0ea2b8dfa26c69d7ce5e3661831fee059
Buyaki <rittahbuyaki@gmail.com>
2022-03-26 11:03:40 +0300

    add tests for the posts controller action index and show, and tests for the show action in the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] 
+  resources :users, only: [:show, :index] do
+    resources :posts, only: [:show, :index] 
   end 
 end 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,14 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET users' do
-    before(:each) { get '/' }
+    before(:each) { get '/users' }
     it 'Should render index template' do
       expect(response).to render_template('users/index')
     end
 
-    before(:each) { get '/users/:user_id' }
+    it 'Should be 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should test placeholder text inside template' do
+      expect(response.body).to include('Users#index')
+    end
+  end
+
+  describe 'GET users' do
+    before(:each) { get '/users/2' }
     it 'Should render show template' do
       expect(response).to render_template('users/show')
+    end
+
+    it 'Should be 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should test placeholder text inside template' do
+      expect(response.body).to include('Users#show')
     end
   end
 end
