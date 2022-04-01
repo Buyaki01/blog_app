84ec9af2537e7669dd7e4d448038ad457938c7eb
Buyaki <rittahbuyaki@gmail.com>
2022-04-01 17:33:41 +0300

    Add form to create post and comment for the current user, add actions, routes and views to allow the user to like a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,27 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @new_post = current_user.posts.new(post_params)
+    respond_to do |format|
+      format.html do
+        if @new_post.save
+          redirect_to "/users/#{@new_post.author.id}/posts/", notice: 'Success!'
+        else
+          render :new, alert: 'Error occured!'
+        end
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: [:show, :index] do
-    resources :posts, only: [:show, :index] 
+    resources :posts, only: [:index, :new, :show, :create] do
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create]
+    end
   end 
 end 
