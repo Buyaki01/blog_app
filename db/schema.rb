3754f1bbc53bd445e5c00df6b10635ad57f7ac2c
Buyaki <rittahbuyaki@gmail.com>
2022-04-02 23:17:46 +0300

    Add default value to zero for the posts_counter in the migration file and updated it on the schema

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_26_203126) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
