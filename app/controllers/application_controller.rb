265763722cd32be38466dfc5bdfa7252a12b8eac
Buyaki <rittahbuyaki@gmail.com>
2022-04-08 12:26:32 +0300

    Fix rubocop errors and add styling to the sign_up and log_in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.includes(:posts).find(params[:id])
-  end 
+  end
 end
