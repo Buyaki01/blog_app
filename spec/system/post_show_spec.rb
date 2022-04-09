00c6d488182dc83e123694ec964bde2f5e80f5e5
Buyaki <rittahbuyaki@gmail.com>
2022-04-09 11:30:06 +0300

    Fix rubocop errors

--- spec/system/post_show_spec.rb
+++ spec/system/post_show_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-​
 RSpec.describe 'Posts show page', type: :feature do
   before :each do
     @user1 = User.create!(
@@ -10,49 +9,37 @@ RSpec.describe 'Posts show page', type: :feature do
       email: 'king@example.com',
       password: 'king123'
     )
-​
     @user2 = User.create!(email: 'janedoe@gmail.com', password: '123abc', name: 'Jane', bio: 'Lorem Ipsum...', photo: 'https://monstar-lab.com/global/wp-content/uploads/sites/11/2019/04/male-placeholder-image.jpeg')
-​
     visit '/users/sign_in'
     fill_in 'Email', with: 'king@example.com'
     fill_in 'Password', with: 'king123'
     click_button 'Log in'
-​
     @post1 = @user1.posts.create!(title: 'Dancing', text: 'Dancing is my hobby', comments_counter: 0, likes_counter: 0)
-​
     @post1.comments.create!(author: @user2, text: 'Woow!')
     @post1.comments.create!(author: @user1, text: 'Same to me')
     @post1.comments.create!(author: @user2, text: 'so true')
     @post1.comments.create!(author: @user1, text: 'Amazing!')
     @post1.comments.create!(author: @user2, text: 'Awesome!')
     @post1.comments.create!(author: @user1, text: 'Perfect!')
-​
     @post1.likes.create!(author: @user1)
     @post1.likes.create!(author: @user2)
-​
     visit user_posts_path(@user1)
   end
-​
   it "should show the post's title" do
     expect(page).to have_content('Dancing')
   end
-​
   it "should show the post's author" do
     expect(page).to have_content('Jane')
   end
-​
   it 'should display the how many comments a post has' do
     expect(page).to have_content('Comments: 6')
   end
-​
   it 'should display the number of likes a post has' do
     expect(page).to have_content('Likes: 2')
   end
-​
   it "should display some of the post's body" do
     expect(page).to have_content('Dancing is my hobby')
   end
-​
   it 'should display the username of each commenter on the post' do
     expect(page).to have_content('Jane')
     expect(page).to have_content('King')
@@ -65,7 +52,6 @@ RSpec.describe 'Posts show page', type: :feature do
     expect(page).to have_content('comment5')
     expect(page).to have_content('comment6')
   end
-​
   it 'should see the create post button and it navigates to the new post page' do
     click_link 'Add new post'
     expect(current_path).to eq(new_user_post_path)
